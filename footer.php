f581ae2662ae57c48ac7301a27e369e6d1b6325b
KazumaOchiai <kazuma06101209@gmail.com>
2023-03-16 17:01:49 +0900

    サンプルサイトの構築のファイルです。私はヘッダーを担当します

---
+++ footer.php
@@ -0,0 +1,12 @@
+    <footer id="footer" class="site-footer">
+        <div class="footer-inner">
+            <?php bloginfo('name'); ?>
+            <?php the_privacy_policy_link(); ?>
+        </div><!--site-info-->
+    </footer><!--#footer -->
+
+</div><!--#page-->
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*ハンバガーメニューのJS*/
+jQuery(function ($) {
+  $(".openbtn").click(function () {
+    //ボタンがクリックされたら
+    $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+  });
+
+  $("#g-nav a").click(function () {
+    //ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass("active"); //ボタンの activeクラスを除去し
+    $("#g-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+  });
+});
+
+// .s_07 .accordion_one
+jQuery(function ($) {
+  //.accordion_oneの中の.accordion_headerがクリックされたら
+  $(".s_07 .accordion_one .accordion_header").click(function () {
+    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+    $(this).next(".accordion_inner").slideToggle();
+    $(this).toggleClass("open");
+  });
+});
