6599b58a0c778d771cbb073f5fd3bc174c99a86c
kazuma1987 <daieirecord.ec@gmail.com>
2023-04-07 16:38:50 +0900

    検索有りの時の編集

--- search.php
+++ search.php
@@ -6,17 +6,17 @@
 
 
             <?php if (have_posts()): ?>
-                <?php
+                <div class="search_result"><?php
                      if (isset($_GET['s']) && empty($_GET['s'])) {
                      echo '検索キーワード未入力'; // 検索キーワードが未入力の場合のテキストを指定
                      } else {
                     echo '“'.$_GET['s'] .'”の検索結果：'.$wp_query->found_posts .'件'; // 検索キーワードと該当件数を表示
                 }
-                ?>
+                ?></div>
                 <ul>
                     <?php while(have_posts()): the_post(); ?>
                     <li>
-                        <a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+                        <a href="<?php the_permalink(); ?>"><?php echo the_post_thumbnail(); ?><?php echo get_the_title(); ?></a>
                     </li>
                     <?php endwhile; ?>
                 </ul>
