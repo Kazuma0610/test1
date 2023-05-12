c24e9c50ef452ee56ebb382172dd3cfa7ee4b531
kazuma1987 <daieirecord.ec@gmail.com>
2023-05-12 22:57:55 +0900

    404ページ修正１

--- 404.php
+++ 404.php
@@ -13,9 +13,33 @@
                     1.  検索して見つける
                 </h2>
                 <div class="error-search">
-                    <p>検索ボックスにお探しのコンテンツに該当するキーワードを入力下さい</p>
+                    <p>検索ボックスにお探しのコンテンツに該当するキーワードを入力</p>
                     <div class="search-box">
                         <?php get_search_form(); ?>
+                    </div>
+                </div>
+                <h2 class="error-h2">
+                    2.  カテゴリーから見つける
+                </h2>
+                <div class="error-cat">
+                    <p>それぞれのカテゴリーの一覧から目的のページを探す</p>
+                    <div class="cat-box">
+                        <ul>
+                            <li><a href="#">sample1</a></li>
+                            <li><a href="#">sample2</a></li>
+                            <li><a href="#">sample3</a></li>
+                            <li><a href="#">sample4</a></li>
+                            <li><a href="#">sample5</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <h2 class="error-h2">
+                    3.  無効なリンクを報告する
+                </h2>
+                <div class="error-report">
+                    <p>当サイトで無効なリンクを発見された場合、お問い合わせからご報告頂けたら幸いでございます</p>
+                    <div class="report-box">
+                        <a href="#">お問合せはコチラから</a>
                     </div>
                 </div>
             </div>
