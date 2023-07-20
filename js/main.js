e5df17f8125e9fbf30bfab2fc839aceb3a74d465
KazumaOchiai <kazuma06101209@gmail.com>
2023-07-20 11:45:57 +0900

    最終修正

--- js/main.js
+++ js/main.js
@@ -197,9 +197,11 @@ $(window).scroll(function () {
 //ボタンを押したらモーダルが出現
 function buttonClick() {
   $(".modal").fadeIn();
+  $("html").addClass("fixed"); // 背景固定させるクラス付与
 }
 
 $(".popup-close").on("click", function () {
+  $("html").removeClass("fixed"); // 背景固定させるクラス付与
   $(".modal").fadeOut();
   return false;
 });
